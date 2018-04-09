2488d23c8a025ad31aee6080bb7f6cf31cddfe23
Abhik <abhiksetia@gmail.com>
2018-04-10 02:06:17 +0530

    all set with db

--- js/credentials.js
+++ js/credentials.js
@@ -12,7 +12,7 @@ function initApp() {
 // Listen for auth state changes.
 // [START authstatelistener]
 
-Materialize.toast('Checking status of sign-in...', 2000);
+Materialize.toast('Checking status of sign-in...', 1000);
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
@@ -25,38 +25,45 @@ firebase.auth().onAuthStateChanged(function(user) {
     var uid = user.uid;
     var providerData = user.providerData;
 
+    Materialize.toast("Welcome "+displayName+",Checking live status....",4000);
     $("#after_login").show();
     $("#before_login").hide();
-    //console.log("writing database");
-    //writeUserData(uid,displayName,email,photoURL);
-    readDB(uid);
+ 
+    writeUserData(uid,displayName,email,photoURL);
+    //readDB(uid);
     // [END_EXCLUDE]
   } else {
     $("#after_login").hide();
-    $("#before_login").show();
-    // Let's try to get a Google auth token programmatically.
-   
+    $("#before_login").show();   
   }
-  //document.getElementById('quickstart-button').disabled = false;
+
 });
 // [END authstatelistener]
 
 document.getElementById('quickstart-button').addEventListener('click', startSignIn, false);
 }
+
 function writeUserData(userId, name, email, imageUrl) {
-firebase.database().ref('users/' + userId).set({
-  username: name,
-  email: email,
-  profile_picture : imageUrl
-});
+
+    var user_data={ username: name,
+      email: email,
+      profile_picture : imageUrl,
+      twitter_username:"",
+      twitter_password:""};
+
+      firebase.database().ref('users/' + userId).transaction(function(currentUserData){
+       if (currentUserData === null){
+          chrome.storage.sync.set({"userid": userId}, function() {
+            console.log("saved user id in chrome storage "+userId);
+          });
+          return user_data;  
+        }
+      }, function(error, committed) {
+        console.log("do not write data again");
+        check_db_for_twitter(userId);
+      });    
 }
 
-function readDB(userId){
-var dburl=firebase.database().ref('users/' + userId);
-dburl.on('value',function(snapshot){
-  console.log(snapshot.val());
-});
-}
 /**
 * Start the auth flow and authorizes to Firebase.
 * @param{boolean} interactive True if the OAuth flow should request with an interactive mode.
@@ -127,16 +134,34 @@ function send_Notification(){
 hr.send(data);
       
 }
+
+function check_db_for_twitter(userId){
+
+  var dburl=firebase.database().ref('users/' + userId);
+  dburl.on('value',function(snapshot){
+    var user_obj=snapshot.val();
+
+    if(user_obj.twitter_password!="" && user_obj.twitter_username!=""){
+      $("#login_details").hide();
+      $("#landing_txt").text("Let's roll....");   
+    }  
+
+  });
+}
+
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+
   var curr_tab=tabs[0].url;
   var urlPattern=new RegExp("https://twitter.com/*");
+
   if(curr_tab.match(urlPattern)){
-    $("#landing_txt").text("Please provide sign in details for one time only.");
 
-    
-
+    $("#landing_txt").text("Please provide sign in details for one time only.");   
+    $("#login_details").show();
+    //Materialize.toast("checking live status, please wait....",2000);
   }else{
     $("#landing_txt").text("Hi,Please open twitter login page to autologin.");
+    $("#login_details").hide();
   }});
 
 window.onload = function() {
